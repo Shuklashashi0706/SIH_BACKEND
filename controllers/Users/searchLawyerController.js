baacfd05132e48f515ffbc3653fb1b85fcab2fae
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-09-18 17:40:46 +0530

    Updated

--- controllers/Users/searchLawyerController.js
+++ controllers/Users/searchLawyerController.js
@@ -10,17 +10,17 @@ export const getAllLawyers = async (req, res) => {
       return res.status(404).json({ message: "No lawyers found" });
     }
 
-    // Prepare a response with relevant lawyer information including ID
-    const lawyerData = lawyers.map((lawyer) => ({
-      ID: lawyer._id, // Include the lawyer ID
-      Name: `${lawyer.firstName} ${lawyer.lastName}`,
-      Location: lawyer.location,
-      "Case Domain": lawyer.caseDomain,
-      "Years Of Experience": new Date().getFullYear() - lawyer.yearOfJoining,
-    }));
+    // // Prepare a response with relevant lawyer information including ID
+    // const lawyerData = lawyers.map((lawyer) => ({
+    //   ID: lawyer._id, // Include the lawyer ID
+    //   Name: `${lawyer.firstName} ${lawyer.lastName}`,
+    //   Location: lawyer.location,
+    //   "Case Domain": lawyer.caseDomain,
+    //   "Years Of Experience": new Date().getFullYear() - lawyer.yearOfJoining,
+    // }));
 
     // Send the list of lawyers with a success message
-    return res.status(200).json({ message: "Successful", lawyers: lawyerData });
+    return res.status(200).json({ message: "Successful", lawyers});
   } catch (error) {
     console.error("Error while fetching lawyers:", error);
     return res.status(500).json({ message: "Internal Server Error" });
@@ -39,14 +39,14 @@ export const getLawyerByLocation = async (req, res) => {
         .json({ message: "No lawyers found in the specified location" });
     }
     // Prepare a response with relevant lawyer information (name and location)
-    const lawyerData = lawyers.map((lawyer) => ({
-      Name: `${lawyer.firstName} ${lawyer.lastName}`,
-      Location: lawyer.location,
-    }));
+    // const lawyerData = lawyers.map((lawyer) => ({
+    //   Name: `${lawyer.firstName} ${lawyer.lastName}`,
+    //   Location: lawyer.location,
+    // }));
     // Send the list of lawyers with the specified location
     return res.status(200).json({
       message: "Lawyers found in the specified location",
-      lawyers: lawyerData,
+      lawyers,
     });
   } catch (error) {
     console.error("Error while fetching lawyers by location:", error);
@@ -58,22 +58,21 @@ export const getLawyerByCaseDomain = async (req, res) => {
     const { caseDomain } = req.body;
     // Find lawyers by case domain in the database
     const lawyers = await LawyerModel.find({ caseDomain });
-    console.log(lawyers);
     if (lawyers.length === 0) {
       // No lawyers found for the specified case domain
       return res
         .status(404)
         .json({ message: "No lawyers found for the specified case domain" });
     }
-    // Prepare a response with relevant lawyer information (name and location)
-    const lawyerData = lawyers.map((lawyer) => ({
-      Name: `${lawyer.firstName} ${lawyer.lastName}`,
-      Location: lawyer.location,
-    }));
+    // // Prepare a response with relevant lawyer information (name and location)
+    // const lawyerData = lawyers.map((lawyer) => ({
+    //   Name: `${lawyer.firstName} ${lawyer.lastName}`,
+    //   Location: lawyer.location,
+    // }));
     // Send the list of lawyers with the specified case domain
     return res.status(200).json({
       message: `Lawyers found for the case domain: ${caseDomain}`,
-      lawyers: lawyerData,
+      lawyers,
     });
   } catch (error) {
     console.error("Error while fetching lawyers by case domain:", error);
